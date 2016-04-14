cbf8a0e58855c87397e288360e0a717cc4d0995d
parikshit chadha <parikshitchadha@gmail.com>
2016-04-15 00:45:56 +0530

    fifth commit i guess

---
+++ src/git_integration/git_class.java
@@ -0,0 +1,10 @@
+package git_integration;
+
+public class git_class {
+	
+	public static void main(String args[])
+	{
+		System.out.println("hi dude and hi girl");
+	}
+	
+}
---
+++ src/git_integration/git_class2.java
@@ -0,0 +1,9 @@
+package git_integration;
+
+public class git_class2 {
+	public static void main(String args[])
+	{
+		System.out.println("hi to the second class");
+	}
+
+}
