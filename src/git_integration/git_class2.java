d43a9d6bbd03c707147748c2caff712ec3a41bc6
parikshit chadha <parikshitchadha@gmail.com>
2016-04-20 16:55:02 +0530

    commit after merge resolving

--- src/git_integration/git_class2.java
+++ src/git_integration/git_class2.java
@@ -1,15 +1,15 @@
 package git_integration;
 
 public class git_class2 {
-	public int i=20;
-	int j=10;
-	int k=40;
-	int l=30;
+	public int i=10;
+	int j=20;
+	int k=30;
+	int l=40;
 	// introduction to second branch
 	public static void main(String args[])
 	{
 		System.out.println("hi to the second class");
-		System.out.println("hi to the new attempt on branch 1");
+		System.out.println("hi to the new attempt ");
 	}
 
 }
