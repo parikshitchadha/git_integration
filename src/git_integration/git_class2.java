b524f1c8d84c1698c42f4aedd49e5cc8c926c0e1
parikshit chadha <parikshitchadha@gmail.com>
2016-04-20 16:43:56 +0530

    commit on the master

--- src/git_integration/git_class2.java
+++ src/git_integration/git_class2.java
@@ -1,9 +1,15 @@
 package git_integration;
 
 public class git_class2 {
+	public int i=10;
+	int j=20;
+	int k=30;
+	int l=40;
+	// introduction to second branch
 	public static void main(String args[])
 	{
 		System.out.println("hi to the second class");
+		System.out.println("hi to the new attempt");
 	}
 
 }
